89dba3642f47ff72a18c34d56b96a2f4ced74f37
Roman Yurchak <rth.yurchak@gmail.com>
2022-12-13 21:47:57 +0100

    Enable pre-commit.ci

--- setup.py
+++ setup.py
@@ -29,10 +29,6 @@ Intended Audience :: Developers
 License :: OSI Approved :: BSD License
 Programming Language :: Python
 Programming Language :: Python :: 3
-Programming Language :: Python :: 3.6
-Programming Language :: Python :: 3.7
-Programming Language :: Python :: 3.8
-Programming Language :: Python :: 3.9
 Topic :: Software Development
 Operating System :: POSIX
 Operating System :: Unix
