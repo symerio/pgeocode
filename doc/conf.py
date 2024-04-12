9cd79bf7591e252b2b6b9a45d6e8b21c7c1b0476
Roman Yurchak <rth.yurchak@gmail.com>
2024-04-12 13:47:08 +0200

    MAINT Fix documentation building

--- doc/conf.py
+++ doc/conf.py
@@ -43,7 +43,7 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
-    "sphinxcontrib.napoleon",
+    "sphinx.ext.napoleon",
     "sphinx.ext.linkcode",
     "myst_parser",
 ]
@@ -83,7 +83,7 @@ release = pgeocode.__version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
---
+++ doc/index.md
@@ -0,0 +1,11 @@
+```{include} ../README.md
+```
+
+```{eval-rst}
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   contributing
+   api
+```
